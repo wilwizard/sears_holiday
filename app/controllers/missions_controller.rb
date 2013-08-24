c9de627f68adcd3b08acfbd18fc5a438b732a50b
wilwizard <wilwizard4@gmail.com>
2013-08-24 15:28:24 -0700

    changed list

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,23 +1,21 @@
 class MissionsController < ApplicationController
 
 	def index
-		@missions = Mission.all
-		render :json => @missions
+		missions = Mission.all
+		render :missions, layout: false, :locals => { missions: missions }
 	end
 
 	def show
 		mission = Mission.find(params[:id])
-		render :partial => 'show', :locals => {mission: mission}
+		render :partial => 'show', :locals => { mission: mission }
 	end
 
-	private
-
-	def create_hash(missions)
-		hash = {}
-		missions.each do |mission|
-			hash[mission.title] = render :partial => 'show', :locals => {mission: mission}
-		end
-		hash
+	def list
+		missions = Mission.all
+		render :list, :layout => false, :locals => { missions: missions }
 	end
+
+
+	
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 SearsHoliday::Application.routes.draw do
 
+  match 'missions/list', to: 'missions#list'
   resources :missions, :only => [:index, :show]
 
 end
