54f03ef9ad91a2fe4604b6298a880c06f26e82b8
wilwizard <wilwizard4@gmail.com>
2013-08-25 12:08:14 -0700

    idk

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,3 @@ File.foreach(File.join(Rails.root, 'missions.csv')) do |mission|
 	Mission.create(:statement => mission.chomp, :title => Faker::Lorem.words(3))
 end
 
-User.create(:email => 'wilwizard4@gmail.com', :password => 'password', :password_confirmation => 'password')
